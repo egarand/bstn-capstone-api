96098981230e9d2d889bae59b1406e78885fba66
Erica Garand <egarand13@outlook.com>
2024-10-17 13:55:09 -0400

    implement GET /pois with mock data

--- controllers/map-controller.js
+++ controllers/map-controller.js
@@ -1,6 +1,220 @@
 
+const exampledata = {
+	"version": 0.6,
+	"generator": "Overpass API 0.7.62.1 084b4234",
+	"osm3s": {
+		"timestamp_osm_base": "2024-10-17T15:44:36Z",
+		"copyright": "The data included in this document is from www.openstreetmap.org. The data is made available under ODbL."
+	},
+	"elements": [
+		{
+			"type": "relation",
+			"id": 6025349,
+			"bounds": {
+				"minlat": 44.1899667,
+				"minlon": -79.3634629,
+				"maxlat": 44.3001117,
+				"maxlon": -79.3214120
+			},
+			"members": [
+				{
+					"type": "way",
+					"ref": 332776901,
+					"role": "",
+					"geometry": [
+						{ "lat": 44.3001117, "lon": -79.3634360 },
+						{ "lat": 44.2986731, "lon": -79.3634508 },
+						{ "lat": 44.2974991, "lon": -79.3634629 },
+						{ "lat": 44.2954765, "lon": -79.3633343 },
+						{ "lat": 44.2895405, "lon": -79.3616627 }
+					]
+				},
+				{
+					"type": "way",
+					"ref": 55970318,
+					"role": "",
+					"geometry": [
+						{ "lat": 44.2891492, "lon": -79.3615590 },
+						{ "lat": 44.2815498, "lon": -79.3595904 },
+						{ "lat": 44.2739230, "lon": -79.3574539 },
+						{ "lat": 44.2643998, "lon": -79.3548874 },
+						{ "lat": 44.2629287, "lon": -79.3542886 },
+						{ "lat": 44.2613792, "lon": -79.3534860 }
+					]
+				},
+				{
+					"type": "way",
+					"ref": 55970312,
+					"role": "",
+					"geometry": [
+						{ "lat": 44.2895405, "lon": -79.3616627 },
+						{ "lat": 44.2891492, "lon": -79.3615590 }
+					]
+				},
+				{
+					"type": "way",
+					"ref": 332775857,
+					"role": "",
+					"geometry": [
+						{ "lat": 44.2608885, "lon": -79.3532693 },
+						{ "lat": 44.2606827, "lon": -79.3531620 }
+					]
+				},
+				{
+					"type": "way",
+					"ref": 55970317,
+					"role": "",
+					"geometry": [
+						{ "lat": 44.2613792, "lon": -79.3534860 },
+						{ "lat": 44.2608885, "lon": -79.3532693 }
+					]
+				},
+				{
+					"type": "way",
+					"ref": 55970308,
+					"role": "",
+					"geometry": [
+						{ "lat": 44.2606827, "lon": -79.3531620 },
+						{ "lat": 44.2591180, "lon": -79.3522564 }
+					]
+				},
+				{
+					"type": "way",
+					"ref": 56000564,
+					"role": "",
+					"geometry": [
+						{ "lat": 44.2591180, "lon": -79.3522564 },
+						{ "lat": 44.2562809, "lon": -79.3509016 },
+						{ "lat": 44.2533678, "lon": -79.3492966 },
+						{ "lat": 44.2533591, "lon": -79.3492922 },
+						{ "lat": 44.2500000, "lon": -79.3475970 },
+						{ "lat": 44.2331282, "lon": -79.3388630 },
+						{ "lat": 44.2273547, "lon": -79.3358768 },
+						{ "lat": 44.2221487, "lon": -79.3331917 }
+					]
+				},
+				{
+					"type": "way",
+					"ref": 504662430,
+					"role": "",
+					"geometry": [
+						{ "lat": 44.2221487, "lon": -79.3331917 },
+						{ "lat": 44.2086726, "lon": -79.3262244 },
+						{ "lat": 44.2054279, "lon": -79.3246205 },
+						{ "lat": 44.2053647, "lon": -79.3246027 },
+						{ "lat": 44.2052077, "lon": -79.3245586 },
+						{ "lat": 44.2035067, "lon": -79.3235848 },
+						{ "lat": 44.2024312, "lon": -79.3230137 },
+						{ "lat": 44.2011185, "lon": -79.3225381 },
+						{ "lat": 44.1899667, "lon": -79.3214120 }
+					]
+				}
+			],
+			"tags": {
+				"hiking": "rwn",
+				"name": "Sutton-Zephyr Rail Trail",
+				"route": "hiking",
+				"type": "route"
+			}
+		},
+		{
+			"type": "way",
+			"id": 850164287,
+			"bounds": {
+				"minlat": 44.3280612,
+				"minlon": -79.3202357,
+				"maxlat": 44.3311765,
+				"maxlon": -79.3136347
+			},
+			"nodes": [
+				7931651348,
+				7931651337,
+				7931651336,
+				7931651335,
+				7931651334,
+				7931651333,
+				7931651332,
+				7931651331,
+				7931651330,
+				7931651329,
+				7931651328,
+				7931651327,
+				7931651326,
+				7931651345,
+				7931651346,
+				7931651347,
+				7931651342,
+				7931651348
+			],
+			"geometry": [
+				{ "lat": 44.3283166, "lon": -79.3186046 },
+				{ "lat": 44.3282731, "lon": -79.3178612 },
+				{ "lat": 44.3281483, "lon": -79.3162756 },
+				{ "lat": 44.3281103, "lon": -79.3150618 },
+				{ "lat": 44.3280612, "lon": -79.3136347 },
+				{ "lat": 44.3286995, "lon": -79.3138942 },
+				{ "lat": 44.3293043, "lon": -79.3143031 },
+				{ "lat": 44.3302377, "lon": -79.3147431 },
+				{ "lat": 44.3308020, "lon": -79.3153501 },
+				{ "lat": 44.3311168, "lon": -79.3159570 },
+				{ "lat": 44.3311711, "lon": -79.3161997 },
+				{ "lat": 44.3311765, "lon": -79.3171936 },
+				{ "lat": 44.3310245, "lon": -79.3186046 },
+				{ "lat": 44.3307912, "lon": -79.3202357 },
+				{ "lat": 44.3293911, "lon": -79.3195378 },
+				{ "lat": 44.3288972, "lon": -79.3191205 },
+				{ "lat": 44.3287344, "lon": -79.3188929 },
+				{ "lat": 44.3283166, "lon": -79.3186046 }
+			],
+			"tags": {
+				"name": "Sibbald Point - Butternut Campground",
+				"tourism": "camp_site",
+				"website": "https://www.ontarioparks.com/park/sibbaldpoint"
+			}
+		}
+	]
+};
+function translateOSMData(element) {
+	const result = {
+		osm_type: element.type,
+		osm_id: element.id,
+		tags: element.tags
+	};
+	if (element.type === "relation") {
+		result.geometry =
+			element.members.map(
+				(member) => member.geometry/*.map(
+					(coord) => [coord.lat, coord.lon]
+				)*/
+			);
+	} else if (element.type === "way") {
+		result.geometry =
+			element.geometry/*.map((coord) => [coord.lat, coord.lon])*/;
+	}
+	return result;
+}
+
+// || ROUTE HANDLERS
+
 export function searchFeatures(req, res) {
-	res.sendStatus(501);
+	// insert request for actual data from overpass here
+	const data = exampledata;
+
+	const result = [];
+
+	for(const elem of data.elements) {
+		const translatedData = translateOSMData(elem);
+		if (elem.tags.leisure?.includes("nature_reserve")) {
+			translatedData.category = "reserve";
+		} else if (elem.tags.tourism?.includes("camp_site") || Object.hasOwn(elem.tags, "camp_site")) {
+			translatedData.category = "campground";
+		} else {
+			translatedData.category = "trail";
+		}
+		result.push(translatedData);
+	}
+
+	res.send(result);
 }
 
 export function getSingleFeature(req, res) {
