75ce106bbba92dc15ef2c20d25f9d898f56f5313
Erica Garand <egarand13@outlook.com>
2024-10-20 16:32:05 -0400

    implement GET /life/:id endpoint

--- controllers/species-controller.js
+++ controllers/species-controller.js
@@ -33,8 +33,32 @@ export async function searchByLocation(req, res) {
 	}
 }
 
-export function getSingleSpecies(req, res) {
-	const { osm_type, osm_id } = req.params;
+export async function getSingleSpecies(req, res) {
+	const { id } = req.params;
+	try {
+		const inatUrl = new URL(`taxa/${id}`, inatBaseUrl);
+		const { data: { results: [taxon] } } = await axios.get(inatUrl);
 
-	res.sendStatus(501);
+		const wikiUrl = new URL(
+			taxon.wikipedia_url.split("/").pop(),
+			"https://en.wikipedia.org/api/rest_v1/page/summary/"
+		);
+		const { data: { extract_html }} = await axios.get(wikiUrl);
+
+		const result = {
+			id: taxon.id,
+			common_name: taxon.preferred_common_name || taxon.english_common_name,
+			scientific_name: taxon.name,
+			photo: {
+				square_url: taxon.default_photo.square_url,
+				medium_url: taxon.default_photo.square_url.replace("square", "medium"),
+				attribution: taxon.default_photo.attribution
+			},
+			wikipedia_url: taxon.wikipedia_url,
+			wikipedia_excerpt: extract_html
+		};
+		res.send(result);
+	} catch (error) {
+		res.status(500).send(error);
+	}
 }
