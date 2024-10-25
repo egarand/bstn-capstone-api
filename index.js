fb68cb049b3ad0cbeba7f34cd73961d4fe303c0a
Erica Garand <egarand13@outlook.com>
2024-10-25 16:23:26 -0400

    add the more lax rate limiter middleware to all routes

--- index.js
+++ index.js
@@ -6,15 +6,19 @@ import compression from "compression";
 import mapRoutes from "./routes/map-routes.js";
 import speciesRoutes from "./routes/species-routes.js";
 import userRoutes from "./routes/user-routes.js";
+import { limiter } from "./middleware/rate-limiting.js";
 
 const PORT = process.env.PORT || 5051;
 const { CORS_ORIGIN } = process.env;
 const app = express();
 
 // global middleware
-app.use(cors({ origin: CORS_ORIGIN }));
-app.use(express.json());
-app.use(compression());
+app.use(
+	cors({ origin: CORS_ORIGIN }),
+	express.json(),
+	compression(),
+	limiter
+);
 
 // routes
 app.use("/pois", mapRoutes);
