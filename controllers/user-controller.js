c20da7c1a7e131871841f0d95e8d6ac77a1bfd20
Erica Garand <egarand13@outlook.com>
2024-10-25 16:25:59 -0400

    implement GET /users/pois

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,8 +1,27 @@
+import initKnex from "knex";
+import knexConfig from "../knexfile.js";
+const knex = initKnex(knexConfig);
 
 // || ROUTE HANDLERS - PoI MANAGEMENT
 
-export function getSavedPois(req, res) {
-	res.sendStatus(501);
+export async function getSavedPois(req, res) {
+	try {
+		// TODO validate JWT token, get real user id, return a 401 if no token, etc
+		const user_id = 1;
+
+		const poiList =
+			await knex("saved_pois")
+				.innerJoin("pois", "saved_pois.poi_id", "pois.id")
+				.where({ "saved_pois.user_id": user_id })
+				.select("pois.*");
+		if (!poiList.length) {
+			res.sendStatus(204);
+		} else {
+			res.send(poiList);
+		}
+	} catch (error) {
+		res.status(500).send(error.message);
+	}
 }
 
 export function savePoi(req, res) {
